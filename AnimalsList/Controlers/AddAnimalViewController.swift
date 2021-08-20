c9f38e3e8e3f728105fadb3ecc1207dca57b0b20
VovaPysarenko <guarder2009@gmail.com>
2021-08-20 19:15:13 +0300

    add tabBarController

--- AnimalsList/Controlers/AddAnimalViewController.swift
+++ AnimalsList/Controlers/AddAnimalViewController.swift
@@ -7,59 +7,51 @@
 
 import UIKit
 import Firebase
-//import FirebaseDatabase
 
 class AddAnimalViewController: UIViewController {
-    
-    var ref: DatabaseReference!
-    private let refDatabase = Database.database().reference().child("Animals")
-
     
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var typeText: UITextField!
     @IBOutlet weak var addAnimalButton: UIButton!
     
-    
-    func saveAnimal() {
-        let animal: [String: Any] = [
-            "name": nameText.text!,
-            "type": typeText.text!
-        ]
-        refDatabase.childByAutoId().setValue(animal)
-    }
-    
-    func clearField() {
-        nameText.text = ""
-        typeText.text = ""
-    }
+    var ref: DatabaseReference!
+    private let refDatabase = Database.database().reference().child("Animals")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-                
-//        self.refDatabase.observeSingleEvent(of: .value) { snapshot in
-//            guard let value = snapshot.value as? [String: Any] else  {
-//                return
-//            }
-//            print("Value: \(value)")
-//        }
         nameText.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
         typeText.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
     }
     
     override func viewWillAppear(_ animated: Bool) {
         addAnimalButton.isEnabled = false
-
     }
     
     @IBAction func addAnimalPressed(_ sender: UIButton) {
         saveAnimal()
         clearField()
     }
+    
+    func saveAnimal() {
+        let animal: [String: Any] = [
+            "name": nameText.text!,
+            "type": typeText.text!
+        ]
+
+        refDatabase.childByAutoId().setValue(animal)
+        
+    }
+    
+    func clearField() {
+        nameText.text = ""
+        typeText.text = ""
+    }
 }
 
 extension AddAnimalViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+      
         return true
     }
     
--- AnimalsList/Controlers/AnimalsListTableViewController.swift
+++ AnimalsList/Controlers/AnimalsListTableViewController.swift
@@ -17,45 +17,20 @@ class AnimalsListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        getAnimals()
 
-//        self.refDatabase.observeSingleEvent(of: .value) { [weak self] snapshot in
-//            guard let value = snapshot.value as? [String: Any] else  {
-//                return
-//            }
-//            print("Value: \(value)")
-//            var _animals = [Animal]()
-//            for item in snapshot.children {
-//                let animal = Animal(snapshot: item as! DataSnapshot)
-//                _animals.append(animal)
-//            }
-//            self?.animals = _animals
-//            self?.tableView.reloadData()
-//            print("Animal: \(self?.animals)")
             
     }
-        func getAnimals() {
-            self.refDatabase.observeSingleEvent(of: .value) { [weak self] snapshot in
-                guard let value = snapshot.value as? [String: Any] else  {
-                    return
-                }
-                print("Value: \(value)")
-                var _animals = [Animal]()
-                for item in snapshot.children {
-                    let animal = Animal(snapshot: item as! DataSnapshot)
-                    _animals.append(animal)
-                }
-                self?.animals = _animals
-                self?.tableView.reloadData()
-        }
-}
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getAnimals()
+    }
+    
 
         
     // MARK: - Table view data source
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return animals.count
     }
 
@@ -70,7 +45,45 @@ class AnimalsListTableViewController: UITableViewController {
 
         return cell
     }
+    
+    
+    // MARK: - Delete row
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return.delete
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+//            let animal = animals[indexPath.row].self
 
+            tableView.beginUpdates()
+            
+            deleteAnimal()
+            
+            animals.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .left)
+            tableView.endUpdates()
+        }
+    }
 
+    func getAnimals() {
+        self.refDatabase.observeSingleEvent(of: .value, with: { [weak self] snapshot in
+            //                guard let value = snapshot.value as? [String: Any] else  {
+            //                    return
+            //                }
+            //                print("Value: \(value)")
+            var _animals = [Animal]()
+            for item in snapshot.children {
+                let animal = Animal(snapshot: item as! DataSnapshot)
+                _animals.append(animal)
+            }
+            self?.animals = _animals
+            self?.tableView.reloadData()
+        })
+    }
+    
+    func deleteAnimal() {
 
+    }
+  
 }
